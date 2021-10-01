89bfbfec3608e1aa8e419cd6b3cbdf49a281d0cb
Josafa Soares de Souza Junior <josafa.souza@serpro.gov.br>
2021-10-01 18:39:14 -0300

    Get verses nethod

--- functions/index.js
+++ functions/index.js
@@ -13,13 +13,15 @@ const app = express();
 app.use(cors);
 
 app.get("/", (req, res) => {
+  const date = "Rm+10:15";
+
   res.send(`
       <!doctype html>
       <head>
         <title>JJDev</title>
       </head>
       <body>
-      Em construção...
+      Em construção... ${date.split(/[+]/)}
       </body>
     </html>`);
 });
@@ -28,6 +30,37 @@ app.get("/api/bible/book/:id", (req, res) => {
   axiosInstance.get(`/books/${req.params.id}`).then((response) => {
     res.json(response.data);
   });
+});
+
+app.get("/api/bible/:version/:key", (req, res) => {
+  const keyArr = req.params.key.split(/[+:-]/);
+  const book = keyArr[0];
+  const chapter = keyArr[1];
+  const firstVerse = keyArr[2];
+  const lastVerse = keyArr.length > 3 ? keyArr[3] : null;
+
+  axiosInstance
+    .get(`/verses/${req.params.version}/${book}/${chapter}`)
+    .then((response) => {
+      const chp = response.data;
+      let finalText = "";
+      chp.verses.forEach((v) => {
+        if (v.number >= firstVerse && v.number <= lastVerse) {
+          finalText += `<strong>${v.number}</strong> ${v.text} `;
+        }
+      });
+      res.send(`<!doctype html>
+      <head>
+        <title>JJDev</title>
+      </head>
+      <body>
+      ${finalText}
+      </body>
+    </html>`);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 exports.app = functions.https.onRequest(app);
