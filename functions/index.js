d93cbac3bff2dc4de1423005cc2ab38cbc3f5ab8
Josafa Soares de Souza Junior <josafa.souza@serpro.gov.br>
2021-10-02 11:32:06 -0300

    Added take daily text from firestore

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const express = require("express");
 const axios = require("axios");
+const { text } = require("express");
 const cors = require("cors")({ origin: true });
+
+admin.initializeApp();
 
 const axiosInstance = axios.create({
   baseURL: "https://www.abibliadigital.com.br/api/",
@@ -12,8 +16,40 @@ const axiosInstance = axios.create({
 const app = express();
 app.use(cors);
 
-app.get("/", (req, res) => {
-  const date = "Rm+10:15";
+app.get("/", async (req, res) => {
+  const date = new Date();
+  const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+  const month = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(
+    date
+  );
+  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+
+  const docRef = admin
+    .firestore()
+    .collection("daily_reading")
+    .doc(`${year}${month}${day}`);
+
+  const doc = await docRef.get();
+
+  if (!doc.exists) {
+    console.error("No such document!");
+  } else {
+    console.info("Document data:", doc.data());
+  }
+
+  const theme = await doc.data().themeRef.get();
+  let questions = "";
+
+  for (let i = 0; i < theme.data().questions.length; i++) {
+    questions += `${i + 1}. ${theme.data().questions[i]}<br>`;
+  }
+
+  let texts = "";
+
+  for (const t of doc.data().text) {
+    const verse = await getText(t);
+    texts += `<strong>${t.replace("+", " ")}</strong><br>${verse}<br><br>`;
+  }
 
   res.send(`
       <!doctype html>
@@ -21,10 +57,31 @@ app.get("/", (req, res) => {
         <title>JJDev</title>
       </head>
       <body>
-      Em construção... ${date.split(/[+]/)}
+        <h1><p>${theme.id} - ${theme.data().title}</p></h1>
+        ${questions}
+        <hr>
+        ${texts}
       </body>
     </html>`);
 });
+
+async function getText(reference) {
+  const keyArr = reference.split(/[+:-]/);
+  const book = keyArr[0].toLowerCase();
+  const chapter = keyArr[1];
+  const firstVerse = keyArr[2];
+  const lastVerse = keyArr.length > 3 ? keyArr[3] : firstVerse;
+
+  const response = await axiosInstance.get(`/verses/ra/${book}/${chapter}`);
+  const chp = response.data;
+  let finalText = "";
+  chp.verses.forEach((v) => {
+    if (v.number >= firstVerse && v.number <= lastVerse) {
+      finalText += `${v.number} ${v.text} `;
+    }
+  });
+  return finalText;
+}
 
 app.get("/api/bible/book/:id", (req, res) => {
   axiosInstance.get(`/books/${req.params.id}`).then((response) => {
@@ -37,7 +94,7 @@ app.get("/api/bible/:version/:key", (req, res) => {
   const book = keyArr[0];
   const chapter = keyArr[1];
   const firstVerse = keyArr[2];
-  const lastVerse = keyArr.length > 3 ? keyArr[3] : null;
+  const lastVerse = keyArr.length > 3 ? keyArr[3] : firstVerse;
 
   axiosInstance
     .get(`/verses/${req.params.version}/${book}/${chapter}`)
